ab7ebd43ad6da7c89a617f8cd61e86101d413a04
Łukasz Jankowski <ukaszjankowski@gmail.com>
2020-05-14 22:28:06 +0200

    Empty implementation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "bmp280.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,6 +53,10 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
+
+void delay_ms(uint32_t period_ms);
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
 
 /* USER CODE END PFP */
 
@@ -92,7 +96,40 @@ int main(void)
   MX_I2C1_Init();
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
+  int8_t rslt;
+  struct bmp280_dev bmp;
 
+  /* Map the delay function pointer with the function responsible for implementing the delay */
+  bmp.delay_ms = delay_ms;
+
+  /* Assign device I2C address based on the status of SDO pin (GND for PRIMARY(0x76) & VDD for SECONDARY(0x77)) */
+  bmp.dev_id = BMP280_I2C_ADDR_PRIM;
+
+  /* Select the interface mode as I2C */
+  bmp.intf = BMP280_I2C_INTF;
+
+  /* Map the I2C read & write function pointer with the functions responsible for I2C bus transfer */
+  bmp.read = i2c_reg_read;
+  bmp.write = i2c_reg_write;
+  rslt = bmp280_init(&bmp);
+  /* Always read the current settings before writing, especially when
+   * all the configuration is not modified
+   */
+  rslt = bmp280_get_config(&conf, &bmp);
+
+  /* configuring the temperature oversampling, filter coefficient and output data rate */
+  /* Overwrite the desired settings */
+  conf.filter = BMP280_FILTER_COEFF_2;
+
+  /* Pressure oversampling set at 4x */
+  conf.os_pres = BMP280_OS_4X;
+
+  /* Setting the output data rate as 1HZ(1000ms) */
+  conf.odr = BMP280_ODR_1000_MS;
+  rslt = bmp280_set_config(&conf, &bmp);
+
+  /* Always set the power mode after setting the configuration */
+  rslt = bmp280_set_power_mode(BMP280_NORMAL_MODE, &bmp);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -102,6 +139,38 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
+	/* Temperature */
+    /* Reading the raw data from sensor */
+	rslt = bmp280_get_uncomp_data(&ucomp_data, &bmp);
+
+	/* Getting the 32 bit compensated temperature */
+	rslt = bmp280_get_comp_temp_32bit(&temp32, ucomp_data.uncomp_temp, &bmp);
+
+	/* Getting the compensated temperature as floating point value */
+	rslt = bmp280_get_comp_temp_double(&temp, ucomp_data.uncomp_temp, &bmp);
+	printf("UT: %ld, T32: %ld, T: %f \r\n", ucomp_data.uncomp_temp, temp32, temp);
+
+	/* Pressure */
+	/* Reading the raw data from sensor */
+    rslt = bmp280_get_uncomp_data(&ucomp_data, &bmp);
+
+	/* Getting the compensated pressure using 32 bit precision */
+	rslt = bmp280_get_comp_pres_32bit(&pres32, ucomp_data.uncomp_press, &bmp);
+
+	/* Getting the compensated pressure using 64 bit precision */
+	rslt = bmp280_get_comp_pres_64bit(&pres64, ucomp_data.uncomp_press, &bmp);
+
+	/* Getting the compensated pressure as floating point value */
+	rslt = bmp280_get_comp_pres_double(&pres, ucomp_data.uncomp_press, &bmp);
+	printf("UP: %ld, P32: %ld, P64: %ld, P64N: %ld, P: %f\r\n",
+		ucomp_data.uncomp_press,
+		pres32,
+		pres64,
+		pres64 / 256,
+		pres);
+
+	bmp.delay_ms(1000); /* Sleep time between measurements = BMP280_ODR_1000_MS */
   }
   /* USER CODE END 3 */
 }
@@ -212,6 +281,59 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+/*!
+ *  @brief Function that creates a mandatory delay required in some of the APIs such as "bmg250_soft_reset",
+ *      "bmg250_set_foc", "bmg250_perform_self_test"  and so on.
+ *
+ *  @param[in] period_ms  : the required wait time in milliseconds.
+ *  @return void.
+ *
+ */
+void delay_ms(uint32_t period_ms)
+{
+    /* Implement the delay routine according to the target machine */
+}
+
+/*!
+ *  @brief Function for writing the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[in] reg_data : Pointer to the data buffer whose value is to be written.
+ *  @param[in] length   : No of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_write(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the I2C write routine according to the target machine. */
+    return -1;
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] i2c_addr : Sensor I2C address.
+ *  @param[in] reg_addr : Register address.
+ *  @param[out] reg_data    : Pointer to the data buffer to store the read data.
+ *  @param[in] length   : No of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval 0 -> Success
+ *  @retval >0 -> Failure Info
+ *
+ */
+int8_t i2c_reg_read(uint8_t i2c_addr, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
+{
+
+    /* Implement the I2C read routine according to the target machine. */
+    return -1;
+}
 
 /* USER CODE END 4 */
 
